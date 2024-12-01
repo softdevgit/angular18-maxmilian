ded50c4f5ae97f1c4a73a425c12e0bf5fb36eea4
softdevgit <deveshpandey.pth1@gmail.com>
2024-12-01 14:47:06 -0700

    Chapter 39

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,15 +8,21 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required:true}) name!: string;
+  // @Input({required: true}) id!: string;
+  // @Input({required: true}) avatar!: string;
+  // @Input({required:true}) name!: string;
+  @Input ({required: true}) user!: {
+    id: string;
+    avatar: string;
+    name: string;
+
+  } 
   @Output() select = new EventEmitter();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
   onSelectUser() {
-    this.select.emit(this.id)
+    this.select.emit(this.user)
   }
 }
